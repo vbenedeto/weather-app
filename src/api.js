1f4367091e0e3be1823b1b74e3430a4fd9d067ad
Vittoria <vbenedetoamerico@gmail.com>
2026-02-19 15:30:56 +0100

    Implement API fetching and data modeling with mock data toggle

---
+++ src/api.js
@@ -0,0 +1,28 @@
+import { mockWeatherData } from "./mockData.js";
+
+const USE_MOCK_DATA = true;
+
+export async function getData(location) {
+  if (USE_MOCK_DATA) {
+    await new Promise(resolve => setTimeout(resolve, 1000));
+    console.log("--- Using Mock Data ---:", mockWeatherData);
+    return mockWeatherData;
+  }
+
+  const url = `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=R7PB32LAMPGJWY2J6BPK68JQN`;
+
+  try {
+    const response = await fetch(url);
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const result = await response.json();
+    console.log(result);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+
--- src/index.js
+++ src/index.js
@@ -1,1 +1,16 @@
 import './styles.css';
+import { getData } from './api.js';
+import { createWeatherModel } from './weatherDataProcessor.js';
+
+async function getWeatherDataObj() {
+  try {
+    const rawData = await getData();
+    const cleanData = createWeatherModel(rawData);
+
+    console.log(cleanData);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+getWeatherDataObj();
