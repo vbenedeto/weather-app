cf5d3eb4a35df35796bcdf11ed1996be7e03704a
Vittoria <vbenedetoamerico@gmail.com>
2026-02-24 11:03:39 +0100

    Update font family and add style for app components

--- src/domController.js
+++ src/domController.js
@@ -1,13 +1,15 @@
-const form = document.getElementById("form");
-const searchInput = document.getElementById("search");
 
 export function setUpEventListeners(onSearch) {
+  const form = document.getElementById("form");
+  const searchInput = document.getElementById("search");
+
   form.addEventListener('submit', (event) => {
     event.preventDefault();
     const location = searchInput.value.trim();
 
     if (location) {
       onSearch(location);
+      searchInput.blur();
     }
   })
 }
